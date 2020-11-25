cdbdc2ddd466aa091ebf7563d144b073b45647e4
toyotaka.kamon <toyotaka-kamon@outlook.jp>
2020-11-25 10:21:12 +0000

    resubmission Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,9 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(20)
   end
 
   def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc).page(params[:page])
-    counts(@user)
   end
 
   def new
